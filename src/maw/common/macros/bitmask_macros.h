060b0694dd431915008f848668ac2791f073f509
Joakim Jalap <joakim.jalap@gmail.com>
2015-03-14 22:46:41 +0100

    All typedefs now have suffix "_t"

--- src/maw/common/macros/bitmask_macros.h
+++ src/maw/common/macros/bitmask_macros.h
@@ -38,17 +38,19 @@
    up with...
 */
 #define MAW_DECL_BITS(pkg_t, field, nrbits, type)			\
-  typedef type field;							\
-  static constexpr unsigned field##_offs = MAW_TOT_BITS + nrbits;		\
-  constexpr field get_##field(const pkg_t in) {				\
-    return (field) ((in & MAW_BITMASK(MAW_TOT_BITS + nrbits -1 , MAW_TOT_BITS)) \
-                    >> MAW_TOT_BITS);                                   \
+  typedef type field##_t;                                               \
+  static constexpr unsigned field##_offs = MAW_TOT_BITS + nrbits;       \
+  constexpr field##_t get_##field(const pkg_t in) {                     \
+    return (field##_t)                                                  \
+      ((in & MAW_BITMASK(MAW_TOT_BITS + nrbits -1 , MAW_TOT_BITS))      \
+       >> MAW_TOT_BITS);                                                \
   }									\
-  constexpr pkg_t set_##field(const pkg_t target, const field mask) {	\
-    return (target & (~(MAW_BITMASK(MAW_TOT_BITS + nrbits -1 , MAW_TOT_BITS)))) | \
+  constexpr pkg_t set_##field(const pkg_t target, const field##_t mask) { \
+    return (target &                                                    \
+      (~(MAW_BITMASK(MAW_TOT_BITS + nrbits -1 , MAW_TOT_BITS)))) |      \
       ((unsigned long long) mask << MAW_TOT_BITS);			\
   }									\
-  inline void set_##field(pkg_t *target, const field mask) {		\
+  inline void set_##field(pkg_t *target, const field##_t mask) {		\
     *target = (*target &                                                \
                ~(MAW_BITMASK(MAW_TOT_BITS + nrbits - 1, MAW_TOT_BITS))) |   \
       ((unsigned long long) mask << MAW_TOT_BITS);			\
--- src/maw/common/macros/test_bitmask_macros.cpp
+++ src/maw/common/macros/test_bitmask_macros.cpp
@@ -23,12 +23,13 @@ MAW_DECL_BITS(test_t, field3, 2, uint8_t)
 #undef LAST_FIELD
 #define LAST_FIELD field3
 
-MAW_DECL_BITS(test_t, field4, 3, uint8_t)
+MAW_DECL_BITS(test_t, field4, 1, bool)
 #undef LAST_FIELD
+#define LAST_FIELD field4
 
 // field{1,2} were typedefed by MAW_DECL_BITS
-enum FIELD1: field1 {F1BE = 0xbe, F1CD = 0xcd};
-enum FIELD2: field2 {F27 = 0x7, F25 = 0x5};
+enum FIELD1: field1_t {F1BE = 0xbe, F1CD = 0xcd};
+enum FIELD2: field2_t {F27 = 0x7, F25 = 0x5};
 
 // test the getter and setters declared by MAW_DECL_BITS.
 // We return 77 in case of a failure because that's what
@@ -37,7 +38,7 @@ int main(int argc, char** argv) {
   
   test_t t1 = 0;
   set_field1(&t1, F1BE);
-  field1 be = get_field1(t1);
+  field1_t be = get_field1(t1);
   if (be != 0xbe) return 77;
   set_field2(&t1, F25);
   if (get_field2(t1) != F25) return 77;
@@ -50,9 +51,10 @@ int main(int argc, char** argv) {
   set_field1(&t2, 0xFF);
   set_field2(&t2, 0x7);
   set_field3(&t2, 0x3);
-  set_field4(&t2, 0x7);
-  if (t2 != 0xFFFF) return 77;
-  if (set_field4(t2, 0x5) != 0xBFFF) return 77;
+  set_field4(&t2, true);
+  if (!get_field4(t2)) return 77;
+  if (t2 != 0x3FFF) return 77;
+  if (set_field4(t2, false) != 0x1FFF) return 77;
   
   return 0;
 }
